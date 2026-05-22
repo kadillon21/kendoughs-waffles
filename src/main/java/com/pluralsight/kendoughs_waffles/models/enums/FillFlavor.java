33fda4bd811fc6d60c8b6300b4e7f47f92c217e9
kadillon21 <kadillon21@gmail.com>
2026-05-22 12:06:08 -0500

    Add enums for SideType and FillFlavor with labels and prices

--- src/main/java/com/pluralsight/kendoughs_waffles/models/enums/FillFlavor.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/enums/FillFlavor.java
@@ -1,4 +1,18 @@
 package com.pluralsight.kendoughs_waffles.models.enums;
 
 public enum FillFlavor {
+    NUTELLA("Nutella"),
+    CREAM_CHEESE("Cream Cheese"),
+    JAM("Strawberry Jam"),
+    STRAWBERRY("Strawberry");
+
+    public final String label;
+
+    FillFlavor(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
--- src/main/java/com/pluralsight/kendoughs_waffles/models/enums/SideType.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/enums/SideType.java
@@ -1,4 +1,24 @@
 package com.pluralsight.kendoughs_waffles.models.enums;
 
 public enum SideType {
+    HASH_BROWNS("Hash Browns", 1.50),
+    WAFFLE_FRIES("Waffle Fries", 1.50),
+    BACON("Bacon", 2.00),
+    FRUIT_CUP("Fruit Cup", 2.00);
+
+    private final String label;
+    private final double price;
+
+    SideType(String label, double basePrice) {
+        this.label = label;
+        this.price = basePrice;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
