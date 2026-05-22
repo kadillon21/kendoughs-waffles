bcdb5b75fdf8669a77e1705662a61a1919546bc5
kadillon21 <kadillon21@gmail.com>
2026-05-22 13:20:12 -0500

    Add Order class with customer and product details; update Product with encapsulation and accessors

--- src/main/java/com/pluralsight/kendoughs_waffles/models/Order.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/Order.java
@@ -1,4 +1,51 @@
 package com.pluralsight.kendoughs_waffles.models;
 
+import java.util.List;
+
 public class Order {
+    private int id;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+    private List<Product> products;
+
+    public Order(int id, String customerName, String customerEmail, String customerPhone, List<Product> products) {
+        this.id = id;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhone = customerPhone;
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double calcTotal() {
+        double total = 0;
+        for(Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
 }
--- src/main/java/com/pluralsight/kendoughs_waffles/models/Product.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/Product.java
@@ -1,8 +1,16 @@
 package com.pluralsight.kendoughs_waffles.models;
 
-public abstract class Product implements Printable{
-    String name;
-    int price;
+public abstract class Product implements Printable {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 
     @Override
     public abstract void printDetails();
