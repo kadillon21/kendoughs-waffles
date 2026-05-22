cfbf349db1b86178a1f408643e33ed6fa63434b0
kadillon21 <kadillon21@gmail.com>
2026-05-22 11:46:32 -0500

    Add labels to WaffleSize enum

--- src/main/java/com/pluralsight/kendoughs_waffles/models/enums/WaffleSize.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/enums/WaffleSize.java
@@ -1,4 +1,17 @@
 package com.pluralsight.kendoughs_waffles.models.enums;
 
 public enum WaffleSize {
+    MINI("Mini"),
+    REGULAR("Regular"),
+    LARGE("Large");
+
+    private final String label;
+
+    WaffleSize(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
