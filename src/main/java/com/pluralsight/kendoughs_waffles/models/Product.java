9b072ebb3b1a01914df98f0d5af0208235e7d401
kadillon21 <kadillon21@gmail.com>
2026-05-22 12:25:36 -0500

    Add Printable interface for model details output

--- src/main/java/com/pluralsight/kendoughs_waffles/models/Product.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/Product.java
@@ -1,4 +1,10 @@
 package com.pluralsight.kendoughs_waffles.models;
 
-public class Product {
+public abstract class Product implements Printable{
+    String name;
+    int price;
+
+    @Override
+    public abstract void printDetails();
+
 }
--- src/main/java/com/pluralsight/kendoughs_waffles/models/Topping.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/Topping.java
@@ -1,4 +1,21 @@
 package com.pluralsight.kendoughs_waffles.models;
 
-public class Topping {
+import com.pluralsight.kendoughs_waffles.models.enums.ToppingName;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "toppings")
+public abstract class Topping {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    private ToppingName name;
+
+    private double price;
+    private int stockCount;
+    private boolean isAvailable;
+
+    public abstract double getExtraCost();
 }
