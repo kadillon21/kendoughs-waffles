e76f2627f594b9f9588d74254197b88a07491065
kadillon21 <kadillon21@gmail.com>
2026-05-22 12:42:05 -0500

    Refactor Side and Drink classes with inheritance, annotations, and new properties for better extensibility

--- src/main/java/com/pluralsight/kendoughs_waffles/models/Drink.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/Drink.java
@@ -1,4 +1,24 @@
 package com.pluralsight.kendoughs_waffles.models;
 
-public class Drink {
+import com.pluralsight.kendoughs_waffles.models.enums.DrinkFlavor;
+import com.pluralsight.kendoughs_waffles.models.enums.DrinkSize;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Entity
+public class Drink extends Product{
+    @Enumerated(EnumType.STRING)
+    private DrinkFlavor flavor;
+
+    @Enumerated(EnumType.STRING)
+    private DrinkSize size;
+
+    private int stockCount;
+    private boolean isAvailable;
+
+    @Override
+    public void printDetails() {
+
+    }
 }
--- src/main/java/com/pluralsight/kendoughs_waffles/models/Side.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/Side.java
@@ -1,4 +1,20 @@
 package com.pluralsight.kendoughs_waffles.models;
 
-public class Side {
+import com.pluralsight.kendoughs_waffles.models.enums.SideType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Entity
+public class Side extends Product{
+    @Enumerated(EnumType.STRING)
+    private SideType type;
+
+    private int stockCount;
+    private boolean isAvailable;
+
+    @Override
+    public void printDetails() {
+
+    }
 }
