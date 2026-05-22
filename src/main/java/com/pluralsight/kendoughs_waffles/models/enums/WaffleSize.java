3bc163f05d104e9730a7b2ff4497a80dcf083542
kadillon21 <kadillon21@gmail.com>
2026-05-22 11:50:12 -0500

    Add basePrice to WaffleSize enum

--- src/main/java/com/pluralsight/kendoughs_waffles/models/enums/WaffleSize.java
+++ src/main/java/com/pluralsight/kendoughs_waffles/models/enums/WaffleSize.java
@@ -1,14 +1,16 @@
 package com.pluralsight.kendoughs_waffles.models.enums;
 
 public enum WaffleSize {
-    MINI("Mini"),
-    REGULAR("Regular"),
-    LARGE("Large");
+    MINI("Mini", 5.00),
+    REGULAR("Regular", 7.00),
+    LARGE("Large", 9.00);
 
     private final String label;
+    private final double basePrice;
 
-    WaffleSize(String label) {
+    WaffleSize(String label, double basePrice) {
         this.label = label;
+        this.basePrice = basePrice;
     }
 
     public String getLabel() {
